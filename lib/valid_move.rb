efd8a2be99b26d005d5d271ba644fd2571fcecfb
Rishav Maskey <rishav.maskey@hotmail.com>
2021-04-19 09:03:39 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,6 +5,8 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken? board, index
+  _value = board[index]
+  
   if _value == '' || _value == " "
     return true
   else 
